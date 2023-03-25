a879771130162cb2350b7d15eeb4b378478d09a1
Victor Dudochkin <dudochkin.victor@gmail.com>
2023-03-26 03:07:09 +0700

    wtf

--- src/prelude/compose.rs
+++ src/prelude/compose.rs
@@ -2,13 +2,6 @@
 ///
 /// Allows to compose functions and closures.
 ///
-/// ```no_run
-/// let chained = checkit
-///   .chain(add_data)
-///   .chain(add_experiments)
-///   .chain(add_data)
-///   .chain(|inout| format!("{input}/README.md"));
-/// ```
 pub trait Compose<In, Out>: Fn(In) -> Out {
     /// `impl Trait` only allowed in function and inherent method return types,
     /// not in trait method return - so its boxed
